05f22c11a9166ba8675baf58d6188018bd32b1fb
satyasri19 <nandamurisatyasri19@gmail.com>
2025-04-20 09:59:28 +0530

    Delete .storybook directory

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  typescript: {
-    check: false,
-    reactDocgen: 'react-docgen-typescript',
-  },
-};
